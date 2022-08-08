ddb08c26373e31ed772ca3e571963d208405e5bc
Lutwiy <lutwiy@gmail.com>
2022-08-08 12:06:31 +0200

    Prepared logic

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { CustomersComponent } from './customers.component';
 
 @NgModule({
   declarations: [CustomersComponent],
   imports: [
+    CommonModule,
+    HttpClientModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { UsersComponent } from './users.component';
 
 @NgModule({
   declarations: [UsersComponent],
   imports: [
+    CommonModule,
+    HttpClientModule,
     RouterModule.forChild([
       {
         path: '',
