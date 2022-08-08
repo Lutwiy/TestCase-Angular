5cf810d7867529e77f1c059d56a85c69dd9dfe1e
Lutwiy <lutwiy@gmail.com>
2022-08-08 12:17:15 +0200

    Fix solution to 2 grids

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { DataService } from '../data.service';
 
+interface IDataItem {
+  name: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  public data$: Observable<string>;
+  public data: IDataItem[];
+  public selected: IDataItem;
 
   constructor(private readonly dataService: DataService) {}
 
   ngOnInit(): void {
-    this.data$ = this.dataService.get();
+    this.dataService.get().subscribe((result: string) => {
+      this.data = [{ id: 1, name: result }];
+    });
+  }
 
-    this.data$.subscribe();
+  public onSelect(item: IDataItem): void {
+    this.selected = item;
   }
 }
